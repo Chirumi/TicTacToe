e1d5ed6ef9a115f6a19697b6df47a3c465035040
michiru <kirittoes@gmail.com>
2022-09-17 11:53:29 +0100

    Make gameboard

---
+++ script.js
@@ -0,0 +1,11 @@
+// Pseudo-code
+// Make the game board (3x3 grid)
+
+// Make eventlistener on click to add ALTERNATING X then O then X etc etc until game over
+// Make an object to store Xs and Os that is placed down
+// Make it impossible to pick index that has a X or O already
+// Each square should correspond to an index number in the above object (1 to 9)
+// Make MULTIPLE win conditions (If 3 Xs or Os in a row on index 1,5,9/1,2,3/1,4,7 etc... = game over)
+// Determine winner by checking if there are 3 Xs in a row (P1 win) or 3 Os in a row (P2 win)
+// Make pop up message (Player 1 or Player 2 has won!)
+// Make reset button similar to the one I did on Rock Paper Scissors
