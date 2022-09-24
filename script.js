8d5b4fc0e6b6b13cd302f8e48408fe4504b6c217
michiru <kirittoes@gmail.com>
2022-09-25 00:03:44 +0100

    Add Module pattern so less code in global scope

--- script.js
+++ script.js
@@ -1,173 +1,177 @@
+// TO DO:
+// ADD GAME TIED MESSAGE IF GAME IS TIED
+
 // DOWNSIDE = SOME CODE ARE ACCESSIBLE GLOBALLY (WHICH IS OPPOSITE OF WHAT THE EXERCISE IS ABOUT)
 // THEREFORE TRY TO OPTIMIZE CODE FOR SECURITY (USING MODULUS, FACTORIES ETC ETC)
 
-let gameBoard = {
-    gamePlay: []
-} 
+let ticTacToe = (function () {
+    let gameBoard = {
+        gamePlay: []
+    } 
 
-let header = document.getElementById("header")
-let footer = document.getElementById("footer")
+    let header = document.getElementById("header")
+    let footer = document.getElementById("footer")
 
-let box1 = document.querySelector(".box1")
-box1.textContent = gameBoard.gamePlay[0]
-let box2 = document.querySelector(".box2")
-box2.textContent = gameBoard.gamePlay[1]
-let box3 = document.querySelector(".box3")
-box3.textContent = gameBoard.gamePlay[2]
-let box4 = document.querySelector(".box4")
-box4.textContent = gameBoard.gamePlay[3]
-let box5 = document.querySelector(".box5")
-box5.textContent = gameBoard.gamePlay[4]
-let box6 = document.querySelector(".box6")
-box6.textContent = gameBoard.gamePlay[5]
-let box7 = document.querySelector(".box7")
-box7.textContent = gameBoard.gamePlay[6]
-let box8 = document.querySelector(".box8")
-box8.textContent = gameBoard.gamePlay[7]
-let box9 = document.querySelector(".box9")
-box9.textContent = gameBoard.gamePlay[8]
+    let box1 = document.querySelector(".box1")
+    box1.textContent = gameBoard.gamePlay[0]
+    let box2 = document.querySelector(".box2")
+    box2.textContent = gameBoard.gamePlay[1]
+    let box3 = document.querySelector(".box3")
+    box3.textContent = gameBoard.gamePlay[2]
+    let box4 = document.querySelector(".box4")
+    box4.textContent = gameBoard.gamePlay[3]
+    let box5 = document.querySelector(".box5")
+    box5.textContent = gameBoard.gamePlay[4]
+    let box6 = document.querySelector(".box6")
+    box6.textContent = gameBoard.gamePlay[5]
+    let box7 = document.querySelector(".box7")
+    box7.textContent = gameBoard.gamePlay[6]
+    let box8 = document.querySelector(".box8")
+    box8.textContent = gameBoard.gamePlay[7]
+    let box9 = document.querySelector(".box9")
+    box9.textContent = gameBoard.gamePlay[8]
 
-let boxes = [box1, box2, box3, box4, box5, box6, box7, box8, box9]
+    let boxes = [box1, box2, box3, box4, box5, box6, box7, box8, box9]
 
-let playerMaker = (name, game) => {
-    return {name, game}
-}
-
-let player1 = playerMaker("Zul", [])
-let player2 = playerMaker("Enemy", [])
-let winner = []
-
-function resetGame () {
-    winner = []
-    gameBoard.gamePlay = []
-    player1.game = []
-    player2.game = []
-
-    let removeMsg = document.getElementById("winMsg")
-    removeMsg.remove()
-
-    boxes = [box1, box2, box3, box4, box5, box6, box7, box8, box9]
-    for (let i = 0; i < boxes.length; i++) {
-        boxes[i].textContent = ""
+    let playerMaker = (name, game) => {
+        return {name, game}
     }
-}
 
-function winMessage (num) {
-    let winMsg = document.createElement("div")
-    winMsg.id = "winMsg"
-    winMsg.textContent = `Player ${num} wins!`
-    header.appendChild(winMsg)
-    winner.push(`P${num}`)
-}
+    let player1 = playerMaker("Zul", [])
+    let player2 = playerMaker("Enemy", [])
+    let winner = []
 
-for (let i = 0; i < boxes.length; i++) { // Alternates X and O, starting with X
-    boxes[i].addEventListener("click", () => {
-        if (boxes[i].textContent === "") {
-            if (gameBoard.gamePlay[gameBoard.gamePlay.length - 1] === "X") {
-                gameBoard.gamePlay.push("O")
-                boxes[i].textContent = "O"
-                player2.game.push(i + 1)
+    function resetGame () {
+        winner = []
+        gameBoard.gamePlay = []
+        player1.game = []
+        player2.game = []
 
-                // Check wincon here 
-                winConditionP1()
-                winConditionP2()
+        let removeMsg = document.getElementById("winMsg")
+        removeMsg.remove()
 
-                // If game over
-                if (winner[0] === "P1" || winner[0] === "P2") {
-                    boxes = []
+        boxes = [box1, box2, box3, box4, box5, box6, box7, box8, box9]
+        for (let i = 0; i < boxes.length; i++) {
+            boxes[i].textContent = ""
+        }
+    }
 
-                    let reset = document.createElement("button")
-                    reset.textContent = "RESET"
-                    footer.appendChild(reset)
+    function winMessage (num) {
+        let winMsg = document.createElement("div")
+        winMsg.id = "winMsg"
+        winMsg.textContent = `Player ${num} wins!`
+        header.appendChild(winMsg)
+        winner.push(`P${num}`)
+    }
+
+    for (let i = 0; i < boxes.length; i++) { // Alternates X and O, starting with X
+        boxes[i].addEventListener("click", () => {
+            if (boxes[i].textContent === "") {
+                if (gameBoard.gamePlay[gameBoard.gamePlay.length - 1] === "X") {
+                    gameBoard.gamePlay.push("O")
+                    boxes[i].textContent = "O"
+                    player2.game.push(i + 1)
+
+                    // Check wincon here 
+                    winConditionP1()
+                    winConditionP2()
+
+                    // If game over
+                    if (winner[0] === "P1" || winner[0] === "P2") {
+                        boxes = []
+
+                        let reset = document.createElement("button")
+                        reset.textContent = "RESET"
+                        footer.appendChild(reset)
+                        
+                        reset.addEventListener("click", () => {
+                            footer.removeChild(reset)
+                            resetGame()
+                        })
+                    }
+
+                } else {
+                    gameBoard.gamePlay.push("X")
+                    boxes[i].textContent = "X"
+                    player1.game.push(i + 1)
+
+                    // Check wincon here
+                    winConditionP1()
+                    winConditionP2()
                     
-                    reset.addEventListener("click", () => {
-                        footer.removeChild(reset)
-                        resetGame()
-                    })
+                    // If game over
+                    if (winner[0] === "P1" || winner[0] === "P2") {
+                        boxes = []
+
+                        let reset = document.createElement("button")
+                        reset.textContent = "RESET"
+                        footer.appendChild(reset)
+
+                        reset.addEventListener("click", () => {
+                            footer.removeChild(reset)
+                            resetGame()
+                        })
+                    }
                 }
+            } 
+        })
+    }
 
-            } else {
-                gameBoard.gamePlay.push("X")
-                boxes[i].textContent = "X"
-                player1.game.push(i + 1)
+    //WINCON Player 1
+    function winConditionP1 () {
+        if (player1.game.includes(1) && player1.game.includes(4) && player1.game.includes(7)) {
+            winMessage(1)
 
-                // Check wincon here
-                winConditionP1()
-                winConditionP2()
-                
-                // If game over
-                if (winner[0] === "P1" || winner[0] === "P2") {
-                    boxes = []
+        } else if (player1.game.includes(1) && player1.game.includes(5) && player1.game.includes(9)) {
+            winMessage(1)
 
-                    let reset = document.createElement("button")
-                    reset.textContent = "RESET"
-                    footer.appendChild(reset)
+        } else if (player1.game.includes(1) && player1.game.includes(2) && player1.game.includes(3)) {
+            winMessage(1)
 
-                    reset.addEventListener("click", () => {
-                        footer.removeChild(reset)
-                        resetGame()
-                    })
-                }
-            }
+        } else if (player1.game.includes(2) && player1.game.includes(5) && player1.game.includes(8)) {
+            winMessage(1)
+
+        } else if (player1.game.includes(3) && player1.game.includes(5) && player1.game.includes(7)) {
+            winMessage(1)
+
+        } else if (player1.game.includes(4) && player1.game.includes(5) && player1.game.includes(6)) {
+            winMessage(1)
+
+        } else if (player1.game.includes(3) && player1.game.includes(6) && player1.game.includes(9)) {
+            winMessage(1)
+
+        } else if (player1.game.includes(7) && player1.game.includes(8) && player1.game.includes(9)) {
+            winMessage(1)
         } 
-    })
-}
+    }
 
-//WINCON Player 1
-function winConditionP1 () {
-    if (player1.game.includes(1) && player1.game.includes(4) && player1.game.includes(7)) {
-        winMessage(1)
+    // WINCON Player 2
+    function winConditionP2 () {
+        if (player2.game.includes(1) && player2.game.includes(4) && player2.game.includes(7)) {
+            winMessage(2)
 
-    } else if (player1.game.includes(1) && player1.game.includes(5) && player1.game.includes(9)) {
-        winMessage(1)
+        } else if (player2.game.includes(1) && player2.game.includes(5) && player2.game.includes(9)) {
+            winMessage(2)
 
-    } else if (player1.game.includes(1) && player1.game.includes(2) && player1.game.includes(3)) {
-        winMessage(1)
+        } else if (player2.game.includes(1) && player2.game.includes(2) && player2.game.includes(3)) {
+            winMessage(2)
 
-    } else if (player1.game.includes(2) && player1.game.includes(5) && player1.game.includes(8)) {
-        winMessage(1)
+        } else if (player2.game.includes(2) && player2.game.includes(5) && player2.game.includes(8)) {
+            winMessage(2)
 
-    } else if (player1.game.includes(3) && player1.game.includes(5) && player1.game.includes(7)) {
-        winMessage(1)
+        } else if (player2.game.includes(3) && player2.game.includes(5) && player2.game.includes(7)) {
+            winMessage(2)
 
-    } else if (player1.game.includes(4) && player1.game.includes(5) && player1.game.includes(6)) {
-        winMessage(1)
+        } else if (player2.game.includes(4) && player2.game.includes(5) && player2.game.includes(6)) {
+            winMessage(2)
 
-    } else if (player1.game.includes(3) && player1.game.includes(6) && player1.game.includes(9)) {
-        winMessage(1)
+        } else if (player2.game.includes(3) && player2.game.includes(6) && player2.game.includes(9)) {
+            winMessage(2)
 
-    } else if (player1.game.includes(7) && player1.game.includes(8) && player1.game.includes(9)) {
-        winMessage(1)
-    } 
-}
-
-// WINCON Player 2
-function winConditionP2 () {
-    if (player2.game.includes(1) && player2.game.includes(4) && player2.game.includes(7)) {
-        winMessage(2)
-
-    } else if (player2.game.includes(1) && player2.game.includes(5) && player2.game.includes(9)) {
-        winMessage(2)
-
-    } else if (player2.game.includes(1) && player2.game.includes(2) && player2.game.includes(3)) {
-        winMessage(2)
-
-    } else if (player2.game.includes(2) && player2.game.includes(5) && player2.game.includes(8)) {
-        winMessage(2)
-
-    } else if (player2.game.includes(3) && player2.game.includes(5) && player2.game.includes(7)) {
-        winMessage(2)
-
-    } else if (player2.game.includes(4) && player2.game.includes(5) && player2.game.includes(6)) {
-        winMessage(2)
-
-    } else if (player2.game.includes(3) && player2.game.includes(6) && player2.game.includes(9)) {
-        winMessage(2)
-
-    } else if (player2.game.includes(7) && player2.game.includes(8) && player2.game.includes(9)) {
-        winMessage(2)
-        
-    } 
-}
-
+        } else if (player2.game.includes(7) && player2.game.includes(8) && player2.game.includes(9)) {
+            winMessage(2)
+            
+        } 
+    }
+})()
 
