c57549e4bda47bb1256d2bab1f8e04f23fdef164
michiru <kirittoes@gmail.com>
2024-01-08 23:30:01 +0000

    Disable game once game finishes and make restart button

--- script.js
+++ script.js
@@ -1,23 +1,55 @@
 /*
 TO DO =
-1) ADD STYLES TO GRID
 2) ADD RESTART BUTTON ONCE THE GAME ENDS TO CLEAR BOARD DISPLAY, BOARD ARRAY AND BOARD SEQ ARRAY
 3) ADD CONGRATS MESSAGE ONCE GAME ENDS OTHERWISE TIE MESSAGE
 4) ALLOW PLAYERS TO ADD NAMES BEFORE THE GAME START, THEN DISPLAY WHO'S TURN IT IS
 */
 const displayBoard = document.querySelector(".board")
+const p = displayBoard.parentNode
+const blockClicks = document.createElement("div")
 const div = document.querySelectorAll(".board > div")
 const startBtn = document.querySelector(".startBtn")
 const title = document.querySelector(".start > div:first-of-type")
+const restartBtn = document.querySelector(".restartBtn")
 
-startBtn.addEventListener("click", () => {
+startBtn.addEventListener("click", startBtnEventListener)
+
+function startBtnEventListener() {
     displayBoard.style.display = "grid"
     startBtn.style.display = "none"
     title.style.display = "none"
-})
+}
+
+function disableGame() {
+    blockClicks.style.pointerEvents = "none"
+    p.replaceChild(blockClicks, displayBoard)
+    blockClicks.appendChild(displayBoard)
+    blockClicks.classList.add("wrapper")
+
+    restartBtn.style.display = "block"
+    restartBtn.addEventListener("click", () => {
+        gameBoard.board = []
+        gameBoard.boardSequence = []
+        gameBoard.gameFinish = false
+
+        // REMOVE WRAPPER THAT PREVENTS CLICKS ON BOARD
+        const wrapperNode = document.querySelector(".wrapper")
+        wrapperNode.replaceWith(...wrapperNode.childNodes)
+
+        div.forEach((x) => {
+            x.textContent = ""
+        })
+    })
+}
+
+const gameBoard = {
+    board: [], 
+    boardSequence: [], // SETS X FIRST THEN O THEN X ETC
+    gameFinish: false
+}
 
 div.forEach((x) => {
-    x.addEventListener("click", () => {
+    function clickEvent() {
         if (gameBoard.boardSequence.length == 0) {
             x.textContent = "X"
             gameBoard.boardSequence.push("X")
@@ -36,88 +68,84 @@ div.forEach((x) => {
 
         winCheck.playerOneWin()
         winCheck.playerTwoWIn()
-        // TIE CONDITION AND GAME OVER ON EACH CLICK
-        if (gameBoard.boardSequence.length == 9) {
-            console.log("GAME TIED!")
+        winCheck.tie()
+        if (gameBoard.gameFinish == true) {
+            disableGame()
         }
-    })
+    }
+    x.addEventListener("click", clickEvent)
 })
-
-const gameBoard = {
-    board: [],
-    boardSequence: []
-}
 
 const winCheck = {
     playerOneWin: () => {
         if (gameBoard.board[0] == "X" && gameBoard.board[1] == "X" && gameBoard.board[2] == "X") {
-            playerOne.score++
-            console.log(`Player One wins! Score: ${playerOne.score}`)
+            playerOneResult()
         }
         else if (gameBoard.board[0] == "X" && gameBoard.board[4] == "X" && gameBoard.board[8] == "X") {
-            playerOne.score++
-            console.log(`Player One wins! Score: ${playerOne.score}`)
+            playerOneResult()
         }
         else if (gameBoard.board[1] == "X" && gameBoard.board[4] == "X" && gameBoard.board[7] == "X") {
-            playerOne.score++
-            console.log(`Player One wins! Score: ${playerOne.score}`)            
+            playerOneResult()          
         }
         else if (gameBoard.board[2] == "X" && gameBoard.board[4] == "X" && gameBoard.board[6] == "X") {
-            playerOne.score++
-            console.log(`Player One wins! Score: ${playerOne.score}`)           
+            playerOneResult()      
         }
         else if (gameBoard.board[3] == "X" && gameBoard.board[4] == "X" && gameBoard.board[5] == "X") {
-            playerOne.score++
-            console.log(`Player One wins! Score: ${playerOne.score}`)            
+            playerOneResult()        
         }
         else if (gameBoard.board[4] == "X" && gameBoard.board[5] == "X" && gameBoard.board[8] == "X") {
-            playerOne.score++
-            console.log(`Player One wins! Score: ${playerOne.score}`)           
+            playerOneResult()         
         }
         else if (gameBoard.board[0] == "X" && gameBoard.board[3] == "X" && gameBoard.board[6] == "X") {
-            playerOne.score++
-            console.log(`Player One wins! Score: ${playerOne.score}`)          
+            playerOneResult()      
         }
         else if (gameBoard.board[6] == "X" && gameBoard.board[7] == "X" && gameBoard.board[8] == "X") {
-            playerOne.score++
-            console.log(`Player One wins! Score: ${playerOne.score}`)           
+            playerOneResult()          
         }
     },
 
     playerTwoWIn: () => {
         if (gameBoard.board[0] == "O" && gameBoard.board[1] == "O" && gameBoard.board[2] == "O") {
-            playerTwo.score++
-            console.log(`Player Two wins! Score: ${playerTwo.score}`)
+            playerTwoResult()
         }
         else if (gameBoard.board[0] == "O" && gameBoard.board[4] == "O" && gameBoard.board[6] == "O") {
-            playerTwo.score++
-            console.log(`Player Two wins! Score: ${playerTwo.score}`)
+            playerTwoResult()
         }
         else if (gameBoard.board[1] == "O" && gameBoard.board[4] == "O" && gameBoard.board[7] == "O") {
-            playerTwo.score++
-            console.log(`Player Two wins! Score: ${playerTwo.score}`)     
+            playerTwoResult()  
         }
         else if (gameBoard.board[2] == "O" && gameBoard.board[4] == "O" && gameBoard.board[6] == "O") {
-            playerTwo.score++
-            console.log(`Player Two wins! Score: ${playerTwo.score}`) 
+            playerTwoResult()
         }
         else if (gameBoard.board[3] == "O" && gameBoard.board[4] == "O" && gameBoard.board[5] == "O") {
-            playerTwo.score++
-            console.log(`Player Two wins! Score: ${playerTwo.score}`)
+            playerTwoResult()
         }
         else if (gameBoard.board[2] == "O" && gameBoard.board[5] == "O" && gameBoard.board[8] == "O") {
-            playerTwo.score++
-            console.log(`Player Two wins! Score: ${playerTwo.score}`)
+            playerTwoResult()
         }
         else if (gameBoard.board[0] == "O" && gameBoard.board[3] == "O" && gameBoard.board[6] == "O") {
-            playerTwo.score++
-            console.log(`Player Two wins! Score: ${playerTwo.score}`)
+            playerTwoResult()
         }
         else if (gameBoard.board[6] == "O" && gameBoard.board[7] == "O" && gameBoard.board[8] == "O") {
-            playerTwo.score++
-            console.log(`Player Two wins! Score: ${playerTwo.score}`)
+            playerTwoResult()
+        }
+    },
+
+    tie: () => {
+        if (gameBoard.boardSequence.length == 9) {
+            gameBoard.gameFinish = true
         }
     }
+}
+
+function playerOneResult() {
+    playerOne.score++
+    gameBoard.gameFinish = true
+}
+
+function playerTwoResult() {
+    playerTwo.score++
+    gameBoard.gameFinish = true
 }
 
 function player(name) {
